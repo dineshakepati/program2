251d62c9244b04d11bbf1a072a2540b5561b7e15
dineshakepati <135491011+dineshakepati@users.noreply.github.com>
2024-01-08 18:06:51 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,13 @@
+class Prime{
+public static void main(String args[]){
+int p=5;
+int c=0;
+for(int i=2;i<=p;i++){
+if(p/i==0){
+c++;}
+}
+if(c==2){
+System.out.println("it is prime");}
+else{
+System.out.println("it is not prime");}
+}}
